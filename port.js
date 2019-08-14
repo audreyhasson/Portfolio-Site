50976657a5daa3f005b95d7fdad258a7ccce0871
UT Apple <utapple@UTs-MacBook-Pro.unleashedtech.lan>
2019-08-14 14:39:54 -0400

    Initial repo commit with all portfolio files

---
+++ port.js
@@ -0,0 +1,46 @@
+console.log('banana');
+function myFunction() {
+  let element = document.getElementById("myDIV");
+  element.classList.toggle("mystyle");
+}
+
+//french horn tabs
+
+function openInfo(evt, infoName) {
+  //Declare all variables
+  var i, tabcontent, tablinks;
+//Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  //Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  for (i = 0; i < tabcontent.length; i++) {
+    if (tabcontent[i].id !== infoName) {
+      if (!tabcontent[i].classList.contains("hideMe")) {
+        tabcontent[i].classList.add("hideMe");
+        tabcontent[i].classList.add("conditionalClose");
+      }
+    }
+  }
+  if(document.getElementById(infoName).classList.contains("hideMe")) {
+    document.getElementById(infoName).classList.remove("hideMe");
+  }
+
+
+  //check the ones belowwith an if statement
+    if (document.getElementById(infoName).classList.contains("conditionalClose")) {
+        document.getElementById(infoName).classList.replace("conditionalClose","conditionalOpen");
+        evt.currentTarget.classList.add("active");
+    } else {
+        document.getElementById(infoName).classList.replace("conditionalOpen","conditionalClose");
+        evt.currentTarget.classList.remove("active");
+    }
+
+  //show the current tab, and add an "active" class to the button that opened the tab
+
+}
+;
